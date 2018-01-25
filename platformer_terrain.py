cca1ada752662d4da3ffb4a1ba22c0795dca05e3
Sean <35079464+Hexpixel@users.noreply.github.com>
2018-01-25 11:58:45 -0500

    Update platformer_terrain.py

--- platformer_terrain.py
+++ platformer_terrain.py
@@ -1,9 +1,20 @@
+import pygame
+import pygelt
+import random
+
+def return_terrain():
 """ for loop for procedural terrain generation. """
+terrain_array = []
 for i in range(0, 10):
   # Put random platforms in game
   # level.append([random.randrange(100, 200), random.randrange(100, 200), random.randrange(0, SCREEN_WIDTH), random.randrange(0, SCREEN_HEIGHT)])
   # first two values is the width and height.
-  level.append([200, 5, random.randrange(0, SCREEN_WIDTH), random.randrange(0, SCREEN_HEIGHT)])
-  level.append([200, 5, random.randrange(0, SCREEN_WIDTH), random.randrange(0, SCREEN_HEIGHT)])
-  level.append([200, 5, random.randrange(0, SCREEN_WIDTH), random.randrange(0, SCREEN_HEIGHT)])
-  level.append([200, 5, random.randrange(0, SCREEN_WIDTH), random.randrange(0, SCREEN_HEIGHT)])
+  terrain_array.append([200, 5, random.randrange(0, SCREEN_WIDTH), random.randrange(0, SCREEN_HEIGHT)])
+  terrain_array.append([200, 5, random.randrange(0, SCREEN_WIDTH), random.randrange(0, SCREEN_HEIGHT)])
+  terrain_array.append([200, 5, random.randrange(0, SCREEN_WIDTH), random.randrange(0, SCREEN_HEIGHT)])
+  terrain_array.append([200, 5, random.randrange(0, SCREEN_WIDTH), random.randrange(0, SCREEN_HEIGHT)])
+  
+  return terrain_array
+
+# call this in platformer_main.py
+#return_terrain()
