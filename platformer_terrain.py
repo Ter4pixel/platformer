9fc8460aeda8bdbc08ad28c7df71807d5a49abe8
Sean <35079464+Hexpixel@users.noreply.github.com>
2018-01-25 11:59:44 -0500

    Update platformer_terrain.py

--- platformer_terrain.py
+++ platformer_terrain.py
@@ -10,9 +10,6 @@ for i in range(0, 10):
   # level.append([random.randrange(100, 200), random.randrange(100, 200), random.randrange(0, SCREEN_WIDTH), random.randrange(0, SCREEN_HEIGHT)])
   # first two values is the width and height.
   terrain_array.append([200, 5, random.randrange(0, SCREEN_WIDTH), random.randrange(0, SCREEN_HEIGHT)])
-  terrain_array.append([200, 5, random.randrange(0, SCREEN_WIDTH), random.randrange(0, SCREEN_HEIGHT)])
-  terrain_array.append([200, 5, random.randrange(0, SCREEN_WIDTH), random.randrange(0, SCREEN_HEIGHT)])
-  terrain_array.append([200, 5, random.randrange(0, SCREEN_WIDTH), random.randrange(0, SCREEN_HEIGHT)])
   
   return terrain_array
 
