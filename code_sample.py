99b1dd3bea967c9f42368502de01aa9a6ce4ad68
Sean <35079464+Hexpixel@users.noreply.github.com>
2018-01-26 11:06:30 -0500

    Create code_sample.py

---
+++ code_sample.py
@@ -0,0 +1,6 @@
+#range function practice
+
+# an array of strings
+words = ['grass', 'dirt', 'stone', 'cobalt']
+for printthewords in range(len(words)):
+  print (printthewords, words[printthewords])
