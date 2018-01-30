0dbd56d8f4c25b8e330f191dfa938e86265c9861
Sean <35079464+Hexpixel@users.noreply.github.com>
2018-01-30 11:14:05 -0500

    Delete code_sample.py

--- code_sample.py
+++
@@ -1,6 +0,0 @@
-#range function practice
-
-# an array of strings
-words = ['grass', 'dirt', 'stone', 'cobalt']
-for printthewords in range(len(words)):
-  print (printthewords, words[printthewords])
